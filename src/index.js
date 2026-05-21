2dd72d443211fc597e2bf1f346768e97913195c7
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-05-21 06:50:29 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ProgrammeAlex from './ProgrammeAlex';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ProgrammeAlex />);
