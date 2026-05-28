ac7bcf2c5c4c08baf008f60bef8843af2dde0d50
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-05-28 05:40:51 +0200

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCN4fnXfl5k37wwKTe1-8sksGuvAAY3XMk",
+  apiKey: "AI45CN4fnXfl5k37wwKTe1-8sksGuvAAY3XMk",
   authDomain: "alemille-b7619.firebaseapp.com",
   projectId: "alemille-b7619",
   storageBucket: "alemille-b7619.firebasestorage.app",
-  messagingSenderId: "282371019831",
-  appId: "1:282371019831:android:079204ca5c87540c329bb2",
+  messagingSenderId: "282371519831",
+  appId: "1:2823432019831:android:079204ca5c87540c329bb2",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
