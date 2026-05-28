cca2668f1e096ab406840abbe25579ea34106dd3
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-05-28 11:32:16 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,26 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      // En production : obfuscation du code
-      ...(process.env.NODE_ENV === 'production'
-        ? [
-            [
-              'babel-plugin-obfuscator',
-              {
-                compact: true,
-                controlFlowFlattening: true,
-                deadCodeInjection: true,
-                debugProtection: true,
-                disableConsoleOutput: true,
-                identifierNamesGenerator: 'hexadecimal',
-                rotateStringArray: true,
-                stringArray: true,
-                stringArrayEncoding: ['base64'],
-                stringArrayThreshold: 0.75,
-              },
-            ],
-          ]
-        : []),
+      // Obfuscation désactivée (babel-plugin-obfuscator n'existe pas sur npm)
     ],
   };
 };
